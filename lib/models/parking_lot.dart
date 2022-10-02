fe38fc57f5817840b91f93234c936004ed0d7753
edgarnier1011 <88921924+edgarnier1011@users.noreply.github.com>
2022-10-01 23:25:25 -0500

    Update parking_lot.dart

--- lib/models/parking_lot.dart
+++ lib/models/parking_lot.dart
@@ -36,7 +36,7 @@ class ParkingLot extends StatelessWidget {
       width: json['width'],
     );
   }
-
+/*   commented this section of code in 10/1/2022 11:22PM, I did not change anything to it.  This is the previous code before I made the change below
   @override
   Widget build(BuildContext context) {
     //ET-9/29/2022 10:35PM, added a singlechildscrollView widget to be able to
@@ -60,4 +60,36 @@ class ParkingLot extends StatelessWidget {
       ),
     );
   }
+  */
+  /*New code made on 10/1/2022 at 11:18PM, This code is to make the border lines of the parking lot.  What I did is I just added Widget CustomPaint and moved it above
+      the widget of Consumer<Appstate>.  This was the only place where the code worked normally, If I place in the last place of the widget tree it throws an error
+      when I select a parking space.  The CustomPaint uses a Parking_Border_Line instance, which is just the coordinates and drawing tool to draw the black lines.
+  */    
+  @override
+  Widget build(BuildContext context) {
+    //ET-9/29/2022 10:35PM, added a singlechildscrollView widget to be able to
+    //    move around lot in case parking spaces do not fit in window
+    //HS-9/30/2022 9:28PM, SingleChildScrollView only scrolls in one direction.
+    //    Instead, used FittedBox to contain parkinglot in window. Scroll and zoom
+    //    taken care of by InteractiveViewer.
+    return Container(
+      width: width.toDouble(),
+      height: height.toDouble(),
+      color: Colors.grey,
+      child: CustomPaint(                       //New widget here   <------
+        painter: Parking_Border_Line(),         //Ends here  <---------
+        child: Consumer<AppState>(
+          builder: (_, value, __) {
+            return Stack(
+              children: [
+                for (ParkingSpace slot in value.parkingSpacess
+                    .where((element) => element.parkingLot == name)) ...[slot]
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+  
 }
