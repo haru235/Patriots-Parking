ffe01bd34e9479500b6d2648b1966311650e9ca5
edgarnier1011 <88921924+edgarnier1011@users.noreply.github.com>
2022-11-14 00:24:43 -0600

    StatisticalData for parking lots
    color changes depending on how many spaces are available.  implemented by parallel arrays.

--- lib/models/Statistical_Data.dart
+++ lib/models/Statistical_Data.dart
@@ -1,33 +1,62 @@
-import 'package:patriots_parking/models/parking_lot.dart';
+
 import 'package:flutter/material.dart';
 
-/*
-This class is to get the statistical data for the parking lots. Is a stateful Widget because the data will be changing as spaces are modified.  
-The widgets to display the data will be  to the right side of the corresping lot once the createState method is properly implemented.  
-If space is false, decrease Available, increase occupied
-If space is true, increase Available, decrease occupied
-Added a method to AppState.dart to get the index of the corresponding Statistical data from the array of Statistical data that is also created in the AppState.dart
-The index will be acquired by comparing the name of space class and Statistical Data class, if their lot number matches.
-Also added additional statements to the togglespace method in Firestore_methods.dart file.  The Data will be changed every time a space is toggled.
-I will have the mentioned code commented until I fill the arrays properly and find any possible bugs that will likely occur when traversing the array of statistical data
-(if no matches are found, add a error message).
 
-
-*/
 class StatisticalData extends StatefulWidget {
-  String parkinglot_Name;
-  int Total;
-  int Available;
-  int Occupied;
+  // String parkinglot_Name;
+  String? id;
+  num? Total;
+  num? Available;
+  num? Occupied;
+  String? parkinglot_Name;
 
   StatisticalData({
-    required this.parkinglot_Name,
+    //required this.parkinglot_Name,
+    this.id = '',
     required this.Total,
     required this.Available,
     required this.Occupied,
+    required this.parkinglot_Name,
     Key? key,
   }) : super(key: key);
 
+  StatisticalData.widgetc(StatisticalData temp) {
+    this.Available = temp.Available;
+    this.Total = temp.Total;
+    this.Occupied = temp.Occupied;
+    this.id = temp.id;
+    this.parkinglot_Name = temp.parkinglot_Name;
+  }
+/*
+  const StatisticalDatawidget ({
+  this.id = '',
+  this.Available = 0,
+  this.Total = 0,
+  this.Occupied = 0,
+}) : super(key: key);
+*/
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'Total': Total,
+      'Available': Available,
+      'Occupied': Occupied,
+      'parking lot': parkinglot_Name
+    };
+  }
+
+  static StatisticalData fromJson(Map<String, dynamic> json) {
+    return StatisticalData(
+      id: json['id'],
+      Total: json['Total'],
+      Available: json['Available'],
+      Occupied: json['Occupied'],
+      parkinglot_Name: json['parking lot'],
+    );
+  }
+
+/*
   void decreaseStatiticalData(int i) {
     this.Available--;
     this.Occupied++;
@@ -38,9 +67,28 @@ class StatisticalData extends StatefulWidget {
     this.Occupied--;
   }
 
+  bool checkColor() {
+    if (this.Available > this.Total / 2)
+      return true;
+    else
+      return false;
+  }
+*/
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  State<StatisticalData> createState() => _StatisticalDataState();
+}
+
+class _StatisticalDataState extends State<StatisticalData> {
+  @override
+  Widget build(BuildContext context) {
+    //StatisticalData? temp;
+    return Container(
+        width: 25,
+        height: 50,
+        decoration: BoxDecoration(
+            color: widget.Available! > widget.Total! / 2
+                ? Colors.blue
+                : Colors.yellow),
+        child: Text('${widget.Available}'));
   }
 }
